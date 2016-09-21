af09bc4566553e1b8f21724094f3280251b1a327
mmenoret <mathilde.menoret@telecom-bretagne.eu>
2016-09-21 15:37:28 +0200

    Update Preproc_pipeline.m

--- Preproc_pipeline.m
+++ Preproc_pipeline.m
@@ -5,3 +5,6 @@ cfgpr.segm=[-0.4 0.6];
 cfgpr.xlsfile='D:/DATA/Comportement/STIM2.xlsx';
 
 [dataclean] = func_preprocessing(cfgpr);
+
+%Les ICA restent encore très parasités par certains bad channels - 
+%Avant de lancer le preprocessing généralisé, il faut identifier les bad channels de chaque sujet puis retester le pipeline
